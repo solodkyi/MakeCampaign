d3f3074f48c61304e91a1d40c3fe11b444da8978
Andrii Solodkyi <andrii.solodkyi@fintech-farm.com>
2025-05-01 19:37:28 +0300

    Initial Commit

---
+++ MakeCampaign/MakeCampaignApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeCampaignApp.swift
+//  MakeCampaign
+//
+//  Created by Andrii Solodkyi on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MakeCampaignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
