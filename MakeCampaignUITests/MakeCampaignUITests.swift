f7fe9de5c579172e86ddf080c33f5883bf89567f
Andrii Solodkyi <andrii.solodkyi@me.com>
2025-10-09 15:28:49 +0300

    fixed tests pipeline

--- MakeCampaignUITests/MakeCampaignUITests.swift
+++ MakeCampaignUITests/MakeCampaignUITests.swift
@@ -25,8 +25,6 @@ final class MakeCampaignUITests: XCTestCase {
     @MainActor
     func testExample() throws {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
@@ -34,8 +32,6 @@ final class MakeCampaignUITests: XCTestCase {
     @MainActor
     func testLaunchPerformance() throws {
         // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+
     }
 }
