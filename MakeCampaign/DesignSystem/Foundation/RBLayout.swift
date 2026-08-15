39a17ed8cf8a3b5f6b98c70fb04ecb8d2a4985a1
Andrii Solodkyi <andrey_solodkiy@me.com>
2026-08-15 18:07:24 +0300

    fix: align robyzbir foundation tokens

--- MakeCampaign/DesignSystem/Foundation/RBLayout.swift
+++ MakeCampaign/DesignSystem/Foundation/RBLayout.swift
@@ -33,8 +33,8 @@ struct RBShadow {
     let x: CGFloat
     let y: CGFloat
 
-    static let card = RBShadow(color: .black.opacity(0.08), radius: 12, x: 0, y: 4)
-    static let artwork = RBShadow(color: .black.opacity(0.14), radius: 16, x: 0, y: 8)
-    static let editor = RBShadow(color: .black.opacity(0.12), radius: 20, x: 0, y: 10)
-    static let floating = RBShadow(color: .black.opacity(0.16), radius: 16, x: 0, y: 8)
+    static let card = RBShadow(color: .black.opacity(0.05), radius: 10, x: 0, y: 2)
+    static let artwork = RBShadow(color: .black.opacity(0.10), radius: 30, x: 0, y: 10)
+    static let editor = RBShadow(color: .black.opacity(0.14), radius: 34, x: 0, y: 14)
+    static let floating = RBShadow(color: .black.opacity(0.22), radius: 26, x: 0, y: 10)
 }
--- MakeCampaign/DesignSystem/Foundation/RBTypography.swift
+++ MakeCampaign/DesignSystem/Foundation/RBTypography.swift
@@ -6,8 +6,8 @@
 import SwiftUI
 
 enum RBTypography {
-    static let screenTitle = Font.system(size: 32, weight: .bold, design: .rounded)
-    static let cardTitle = Font.system(size: 18, weight: .semibold, design: .rounded)
-    static let money = Font.system(size: 24, weight: .bold, design: .monospaced)
-    static let microLabel = Font.system(size: 11, weight: .semibold, design: .default)
+    static let screenTitle = Font.system(size: 30, weight: .heavy, design: .default)
+    static let cardTitle = Font.system(size: 16, weight: .bold, design: .default)
+    static let money = Font.system(size: 30, weight: .semibold, design: .monospaced)
+    static let microLabel = Font.system(size: 11, weight: .medium, design: .default)
 }
