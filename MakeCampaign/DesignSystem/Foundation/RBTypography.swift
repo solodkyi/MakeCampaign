63e977062ed20f563572d6d81f527376c24b4dcf
Andrii Solodkyi <andrey_solodkiy@me.com>
2026-08-15 18:09:38 +0300

    fix: use monospaced micro typography

--- MakeCampaign/DesignSystem/Foundation/RBTypography.swift
+++ MakeCampaign/DesignSystem/Foundation/RBTypography.swift
@@ -9,5 +9,5 @@ enum RBTypography {
     static let screenTitle = Font.system(size: 30, weight: .heavy, design: .default)
     static let cardTitle = Font.system(size: 16, weight: .bold, design: .default)
     static let money = Font.system(size: 30, weight: .semibold, design: .monospaced)
-    static let microLabel = Font.system(size: 11, weight: .medium, design: .default)
+    static let microLabel = Font.system(size: 11, weight: .medium, design: .monospaced)
 }
