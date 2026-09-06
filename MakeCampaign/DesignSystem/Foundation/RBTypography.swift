5f142edcbc97a18259425846f11c42be49e341ae
Andrii Solodkyi <andrey_solodkiy@me.com>
2026-09-06 17:28:14 +0300

    fix: integrate design system dependencies

--- MakeCampaign/DesignSystem/Foundation/RBTypography.swift
+++ MakeCampaign/DesignSystem/Foundation/RBTypography.swift
@@ -6,8 +6,8 @@
 import SwiftUI
 
 enum RBTypography {
-    static let screenTitle = Font.system(size: 30, weight: .heavy, design: .default)
-    static let cardTitle = Font.system(size: 16, weight: .bold, design: .default)
-    static let money = Font.system(size: 30, weight: .semibold, design: .monospaced)
-    static let microLabel = Font.system(size: 11, weight: .medium, design: .monospaced)
+    static let screenTitle = SwiftUI.Font.system(size: 30, weight: .heavy, design: .default)
+    static let cardTitle = SwiftUI.Font.system(size: 16, weight: .bold, design: .default)
+    static let money = SwiftUI.Font.system(size: 30, weight: .semibold, design: .monospaced)
+    static let microLabel = SwiftUI.Font.system(size: 11, weight: .medium, design: .monospaced)
 }
